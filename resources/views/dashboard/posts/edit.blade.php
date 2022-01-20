c1e237757b34f5f47eb8121cb101b4fe36b7043b
unknown <fauzan.alghifari17095@student.unsika.ac.id>
2022-01-20 09:39:50 +0700

    Latihan laravel ke sebelas

---
+++ resources/views/dashboard/posts/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Edit Post</h1>
+    </div>
+    <div class="col-lg-8">
+    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+        @method('put')
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="from-label">Title</label>
+            <input type="title" class="form-control @error('title') is-invalid @enderror"  id="title" name="title" value="{{ old ('title', $post->title) }}">
+            @error('title')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="slug" class="from-label">Slug</label>
+            <input type="slug" class="form-control @error('slug') is-invalid @enderror"  id="slug" name="slug"  value="{{ old ('title', $post->slug) }}" disable readonly>
+            @error('slug')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="category" class="from-label">Category</label>
+            <br>
+            <select class="form-select" name="category_id">
+                @foreach($categories as $category)
+                    @if(old ('category_id', $post->category_id) == $category->id)
+                        <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                    @else
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endif
+                @endforeach
+            </select>
+                 </div>
+        <div>
+        <label for="body" class="from-label">Body</label>
+            <input id="body" type="hidden" name="body" value="{{ old ('body', $post->body) }}">
+            @error('body')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
+            <trix-editor input="body"></trix-editor>
+            <label></label>
+        </div>
+        <button type="submit" class="btn btn-warning">Update</button>
+    </form>
+    </div>
+
+    <script>
+        {{--JAVASCRIPT untuk menjalankan slug--}}
+        const title = document.querySelector('#title');
+        const slug = document.querySelector('#slug');
+
+        title.addEventListener ('change', function() {
+            fetch('/dashboard/posts/checkSlug?title=' + title.value)
+                .then (response => response.json())
+                .then (data => slug.value = data.slug)
+
+        });
+
+        // JAVASCRIPT untuk menghilangkan fungsi tambah file
+        document.addEventListener('trix-file-accept', function (e){
+            e.preventDefault();
+        })
+    </script>
+@endsection
---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,33 @@
+@extends('dashboard.layouts.main')
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">My Post (Name : {{ auth()->user()->name }})</h1>
+    </div>
+
+        <div class="row my-3">
+            <div class="col-lg-8">
+                <h3>
+                    <h1 class="mb-3">{{ $post->title }}</h1>
+                </h3>
+                <div class="mb-3"><h15 class="card-text"><small class="text-muted">cobaaplikasi-laravel.test | {{ $post->created_at }}</small></h15></div>
+                <img src="https://source.unsplash.com/1200x400? {{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" class="img-fluid">
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+
+                    <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back</a>
+                    <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                        @method('delete')
+                        @csrf
+                        <button class="btn btn-danger" onclick="return confirm('Are you sure?')">
+                            <span data-feather="x-circle"></span> Delete
+                        </button>
+                    </form>
+                </article>
+            </div>
+        </div>
+
+
+
+
+@endsection
